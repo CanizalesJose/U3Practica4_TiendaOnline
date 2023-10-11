af4c436fbdbf8111159dbc3769db1da1bf3a994c
CanizalesJose <jose1605m@gmail.com>
2023-10-11 11:36:06 -0700

    v2 Tienda funcional y ticket generado funcional

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     const catalogo = [
-        { id: 1, imagen: "https://images.pexels.com/photos/687811/pexels-photo-687811.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", precio: 10 },
-        { id: 2, imagen: "https://images.pexels.com/photos/7610446/pexels-photo-7610446.jpeg?auto=compress&cs=tinysrgb&w=600", precio: 15 },
-        { id: 3, imagen: "https://images.pexels.com/photos/1542252/pexels-photo-1542252.jpeg?auto=compress&cs=tinysrgb&w=600", precio: 20 },
-        // Agrega más productos aquí
+        { id: 1, imagen: "https://images.pexels.com/photos/687811/pexels-photo-687811.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", precio: 10 , nombre: "Control"},
+        { id: 2, imagen: "https://images.pexels.com/photos/7610446/pexels-photo-7610446.jpeg?auto=compress&cs=tinysrgb&w=600", precio: 15, nombre: "Laptop"},
+        { id: 3, imagen: "https://images.pexels.com/photos/1542252/pexels-photo-1542252.jpeg?auto=compress&cs=tinysrgb&w=600", precio: 20, nombre: "Telefono"},
+        { id: 4, imagen :"https://images.pexels.com/photos/8000587/pexels-photo-8000587.jpeg?auto=compress&cs=tinysrgb&w=600", precio: 53, nombre: "Audifonos"}
     ];
 
     const catalogoContainer = document.getElementById("catalogo");
@@ -18,12 +18,12 @@ document.addEventListener("DOMContentLoaded", function () {
             <div class="card">
                 <img src="${producto.imagen}" class="card-img-top imgArticulo" alt="Producto ${producto.id}">
                 <div class="card-body">
-                    <h5 class="card-title">Producto ${producto.id}</h5>
+                    <h5 class="card-title">${producto.id}: ${producto.nombre}</h5>
                     <p class="card-text">Precio: ${producto.precio}
                     <br>
                     Cantidad: <input type="number" class="cantidad" id="cantidadProducto${producto.id}">
                     <br><br>
-                    
+                    </p>
                     <button type="button" class="btn btn-primary agregar"> Agregar al Carrito </button>
                 
             
@@ -42,17 +42,6 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     const carrito = [];
-
-    const finalizarCompra = document.querySelector("#finalizarCompra");
-    finalizarCompra.addEventListener("click", function() {
-        //Al finalizar la compra
-        /*Pasar los datos a la memoria local*/
-        //Borra el carrito
-        carrito.length = 0;
-        actualizarResumenCompra();
-
-        //location.reload();
-    });
 
     function agregarProductoAlCarrito(producto, cantidad) {
         // Busca si el producto ya está en el carrito
@@ -90,4 +79,35 @@ document.addEventListener("DOMContentLoaded", function () {
         // Actualiza el total
         total.textContent = `$${subtotalTotal}`;
     }
+
+    const finalizarCompra = document.querySelector("#finalizarCompra");
+    finalizarCompra.addEventListener("click", function() {
+        //Al finalizar la compra
+        /*Pasar los datos a la memoria local*/
+        /*Crea un contenedor vacio para almacenar los datos del ticket */
+        const arregloEnvio = [];
+        /*Crea las variables que contendra el objeto*/
+        var idArt, nombreArt, precioArt, cantidadArt;
+        /*Por cada elemento del carrito recolecta los datos del producto y guardar en objeto*/
+        carrito.forEach(element => {
+           idArt = element.producto.id;
+           nombreArt = element.producto.nombre;
+           precioArt = element.producto.precio;
+           cantidadArt = element.cantidad;
+           /*Crea el objeto con los datos del producto*/
+           const envio = {idArt, nombreArt, precioArt, cantidadArt};
+           /*Guarda el objeto en el arreglo*/
+           arregloEnvio.push(envio);
+        });
+        /*Mandar el arreglo creado a la memoria local*/
+        localStorage.setItem("arregloEnvio", JSON.stringify(arregloEnvio));
+
+        //Borra el carrito
+        carrito.length = 0;
+        //Actualiza la lista de compra, borrandola
+        actualizarResumenCompra();
+
+        //Abrir ticket
+        window.location.href = "ticket.html";
+    });
 });
---
+++ scriptTicket.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+    //Recuperar la tabla en el documento
+    const infoTicket = document.getElementById("infoTicket");
+    //Recuperar los datos de la compra
+    const compra = JSON.parse(localStorage.getItem("arregloEnvio")) || [];
+
+    var totProd = 0;
+    var totComp = 0;
+    //Recorre cada producto de la compra y lo coloca en la tabla
+    compra.forEach(element => {
+        const fila = document.createElement("tr");
+        fila.innerHTML = `
+            <td>${element.cantidadArt}</td>
+            <td>${element.nombreArt}</td>
+            <td>$${element.precioArt}</td>
+        `;
+        infoTicket.appendChild(fila);
+        totProd = totProd + element.cantidadArt;
+        totComp = totComp + (element.cantidadArt * element.precioArt);
+    });
+
+    //Calcula el total de la venta y de articulos
+    const totalCompra = document.querySelector("#totalCompra");
+    const totalProductos = document.querySelector("#totalProductos");
+
+    totalCompra.textContent = "Total: $"+totComp;
+    totalProductos.textContent = "QTY Total: " + totProd;
+
+
+});
